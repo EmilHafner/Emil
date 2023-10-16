2e5a0d62aca52bd12b8a2d452a42683665da498d
Aronlukas <hafner.emil@gmail.com>
2023-10-17 00:13:36 +0200

    Added Footer placeholder

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 
 export default function Home() {
     return (
-        <main className="flex justify-center h-screen bg-stone-100">
+        <main className="flex flex-col justify-center h-screen bg-stone-100">
             <div className={"flex justify-center items-center"}>
                 <h1 className={"text-8xl text-stone-800 font-medium"}>Emil Hafner</h1>
             </div>
-
+            <div className={"absolute bottom-0 left-1/2 -translate-x-1/2"}>
+                Footer
+            </div>
         </main>
     )
 }
