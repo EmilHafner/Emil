422926801648ee6281d2c39e55433dc560c9c8c3
Aronlukas <hafner.emil@gmail.com>
2023-10-31 00:05:44 +0100

    Font size responsive

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         <main className="flex flex-col justify-center h-screen bg-stone-100 dark:bg-slate-800 text-stone-900 dark:text-stone-200">
             <div className={"flex flex-col gap-4 justify-center items-center"}>
                 <h1 className={"text-6xl md:text-8xl font-semibold text-center"}>Emil Hafner</h1>
-                <p className={"text-xl font-light"}>Full-Stack Software Developer</p>
+                <p className={"text-xl font-light text-center"}>Full-Stack Software Developer</p>
             </div>
             <div className={"flex justify-center mt-20"}>
                 <ul className={"flex flex-row gap-4 text-xl md:text-2xl"}>
