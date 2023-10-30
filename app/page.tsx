56456de937945956bfc3a142e6c56b5d258427c1
Aronlukas <hafner.emil@gmail.com>
2023-10-31 00:03:11 +0100

    Font size responsive

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 
 export default function Home() {
 
@@ -10,11 +9,11 @@ export default function Home() {
     return (
         <main className="flex flex-col justify-center h-screen bg-stone-100 dark:bg-slate-800 text-stone-900 dark:text-stone-200">
             <div className={"flex flex-col gap-4 justify-center items-center"}>
-                <h1 className={"text-8xl font-semibold"}>Emil Hafner</h1>
+                <h1 className={"text-6xl md:text-8xl font-semibold text-center"}>Emil Hafner</h1>
                 <p className={"text-xl font-light"}>Full-Stack Software Developer</p>
             </div>
             <div className={"flex justify-center mt-20"}>
-                <ul className={"flex flex-row gap-4 text-2xl"}>
+                <ul className={"flex flex-row gap-4 text-xl md:text-2xl"}>
                     {links.map((link) => (
                         <li key={link.name}>
                             <a href={link.url.toString()} target={"_blank"}>
