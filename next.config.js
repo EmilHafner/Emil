e2591977e0bb8fe2a0a578ddb4c9f95b9ba62614
Aronlukas <hafner.emil@gmail.com>
2023-11-07 00:49:19 +0100

    cv + redirect from /cv to /cv.pdf

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async redirects(){
+        return [
+        {
+            source: '/cv',
+            destination: '/cv.pdf',
+            permanent: true,
+        },
+    ]}
+}
 
 module.exports = nextConfig
